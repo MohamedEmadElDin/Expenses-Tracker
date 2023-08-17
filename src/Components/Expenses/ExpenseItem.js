33358467b9080198e154ff9f3afc707b9e1a807b
Mohamed-EmadEldin <mohamed.emad1719@gmail.com>
2023-08-17 12:51:43 +0300

    change all the function to arrow function

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,8 +1,8 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
-function ExpenseItem(props){
 
+const ExpenseItem =(props)=>{
     return(
         <Card className='expense-item'>
            <ExpenseDate date ={props.date}/>
@@ -14,3 +14,4 @@ function ExpenseItem(props){
         );
 }
 export default ExpenseItem;
+
