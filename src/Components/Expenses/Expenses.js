0490194aa987077fe790ec953f10795c1c50565e
Mohamed-EmadEldin <mohamed.emad1719@gmail.com>
2023-08-30 19:43:20 +0300

    add new component ExpensesList to make the Expenses component much leaner

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react';
 import './Expenses.css';
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 
 
 const Expenses =(props) => {
@@ -16,18 +16,7 @@ const Expenses =(props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expenseContent = <p>No expenses found</p>
-
-  if (filteredExpenses.length > 0){
-    expenseContent = filteredExpenses.map((expense) =>(
-      <ExpenseItem
-         key = {expense.id}
-         title = {expense.title}
-         amount = {expense.amount}
-         date = {expense.date}
-      />
-      ));
-  }
+  
 
 return (
   <div>
@@ -36,7 +25,7 @@ return (
         selected ={filteredYear}
         onYearChange = {filterChangeHandler}
       />
-      {expenseContent}
+      <ExpensesList items = {filteredExpenses}/>
     </Card>
   </div>
 );
